c2b8aecf48add1f93f507c1a5cee68b602e7e029
araujo.marlon@hotmail.com <araujo.marlon@hotmail.com>
2024-07-27 22:20:39 -0300

    test include

--- 02-Application/AssetPurchaseHandler.cs
+++ 02-Application/AssetPurchaseHandler.cs
@@ -1,17 +1,20 @@
 ﻿using InvestmentManagement.Application.Interfaces;
 using InvestmentManagement.Controllers.V1.Dtos;
+using InvestmentManagement.Infrastructure.Repositories.Querys;
 
 namespace InvestmentManagement.Application
 {
     public class AssetPurchaseHandler : IAssetPurchaseHandler
     {
-        public AssetPurchaseHandler()
+        private readonly UserQueryRepository _user;
+        public AssetPurchaseHandler(UserQueryRepository user)
         {
+            _user = user;
         }
 
-        public async Task AssetPurchaseAsync(AssetPurchaseDto dto)
+        public async Task AssetPurchaseAsync(AssetPurchaseDto dto, string userName)
         {
-
+            var user = await _user.GetUserByUserName(userName) ?? throw new Exception("Usuário inválido");
         }
     }
 }
---
+++ 04-Infrastructure/Repositories/Querys/AssetQueryRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace InvestmentManagement.Infrastructure.Repositories.Querys
+{
+	public class AssetQueryRepository
+	{
+		public AssetQueryRepository()
+		{
+		}
+	}
+}
+
